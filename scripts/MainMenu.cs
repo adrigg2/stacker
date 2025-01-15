3bb22d3b7444f8fdd9c84c3028741442b3d96b77
adrigg2 <adriangarcia2.agg@gmail.com>
2025-01-15 18:24:45 +0100

    finished main menu (for now)

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -13,10 +13,14 @@ public partial class MainMenu : Control
 	[Export]
     private Button _settingsButton;
 
+    [Export]
+    private Button _customizeButton;
+
     public override void _Ready()
 	{
         _startButton.Pressed += () => GetTree().ChangeSceneToFile("res://scenes/game.tscn");
         _exitButton.Pressed += () => GetTree().Quit();
         _settingsButton.Pressed += () => GD.Print("Settings button pressed");
+        _customizeButton.Pressed += () => GD.Print("Customize button pressed");
     }
 }
